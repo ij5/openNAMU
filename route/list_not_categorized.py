9e7c212117a10bb7e2ecd19c119f2287832ca81b
Jaehee Lee <jhlee838@gmail.com>
2022-12-03 16:14:44 +0900

    공백 문서 제거

--- route/list_not_categorized.py
+++ route/list_not_categorized.py
@@ -12,6 +12,8 @@ def list_not_categorized():
 
         curs.execute(db_change('SELECT DISTINCT title FROM history WHERE title NOT LIKE "category:%" AND title NOT IN (SELECT link FROM back WHERE type = "cat") limit 50'))
         for data in curs.fetchall():
+            if not data[0]:
+                continue
             div += '<li>' + '<a href="/w/' + url_pas(data[0]) + '">' + html.escape(data[0]) + '</a></li>'
 
         div += '</ul>'
